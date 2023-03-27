1fb6c608f6224f0c1afa8c354a2696f90ce477f0
Ajay <ajaykumarbagwan2@gmail.com>
2023-03-28 01:59:58 +0530

    First Commit

---
+++ 2nd_index.php
@@ -0,0 +1,4 @@
+<?php
+echo "this is our first php file";
+echo 18+75;
+?>
---
+++ 5th_variable.php
@@ -0,0 +1,17 @@
+<?php
+// keywords are not case sensitive in php.
+// but variables are case sensitive in php.
+
+// variables:
+/*
+variabales are container for storing information.
+variables start with $ sign.
+variables can be alphabetic or alphanumeric.
+variables can not be start with a number.
+variables can not contain white spaces.
+*/
+
+$name = "Ajay kumar";
+$income = 20000000000;
+echo "my name is $name and my total income of a year is $income";
+?>
